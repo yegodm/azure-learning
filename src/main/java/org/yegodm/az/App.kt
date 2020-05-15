d122d86a243d24452d731f6c8a30969e88bc3662
yegodm <dyegorov@yandex.ru>
2020-05-15 19:05:50 -0400

    switched logging to log4j2;

--- src/main/java/org/yegodm/az/App.kt
+++ src/main/java/org/yegodm/az/App.kt
@@ -5,9 +5,10 @@ import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
+import kotlin.reflect.jvm.javaMethod
 
 
-val log = LoggerFactory.getLogger("app")
+val log = LoggerFactory.getLogger(::main.javaMethod!!.declaringClass)
 
 fun main() {
     log.info("Starting service...")
